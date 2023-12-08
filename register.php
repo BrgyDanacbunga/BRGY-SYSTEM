32652e06ddb34b5d6031dfe00a772cd8234535a1
BrgyDanacbunga <153062767+BrgyDanacbunga@users.noreply.github.com>
2023-12-08 16:04:04 +0800

    Update register.php

--- register.php
+++ register.php
@@ -4,9 +4,6 @@ $dbname = 'registerformdb';
 $user = 'root';
 $password = '';
 
-// Debugging: Output the entire $_POST array
-var_dump($_POST);
-
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
