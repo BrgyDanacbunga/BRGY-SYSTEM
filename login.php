c27cde6c7f9cc5ad8d98fa844312548c821f5483
BrgyDanacbunga <153062767+BrgyDanacbunga@users.noreply.github.com>
2023-12-10 12:46:17 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,8 @@
 <?php
 $host = 'localhost';
-$dbname = 'registerformdb';
-$user = 'root';
-$password = '';
-
+$dbname = 'id21642497_brgysystem';
+$user = 'id21642497_systemdb';
+$password = 'DanacSystem@787';
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
